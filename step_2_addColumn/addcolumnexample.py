df685721fffeedbd2a60e9b5613441ef0f969ecc
Kamran Noorian <kamrannoorian@comcast.net>
2016-12-06 22:03:33 -0500

    Update addcolumnexample.py

--- step_2_addColumn/addcolumnexample.py
+++ step_2_addColumn/addcolumnexample.py
@@ -2,7 +2,7 @@ from dataAccess import dataAccess as DB
 import dateTime as DT
 
 #path to filled out Database that has already been created with patient information from the provided CSVs
-dbpath = “lab.db”
+dbpath = “Kate.db”
 
 #connect to the Database by creating an instance of dataAccess
 kateDB = DB(“Kate”, dbpath)
